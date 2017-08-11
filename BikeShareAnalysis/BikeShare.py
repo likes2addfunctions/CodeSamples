14d565a991b19b4fc5d7cf7d6037e0b20fb4a042
likes2addfunctions <galen.novello@gmail.com>
2017-08-10 17:22:24 -0700

    stylized output of bike share

--- BikeShareAnalysis/BikeShare.py
+++ BikeShareAnalysis/BikeShare.py
@@ -6,33 +6,29 @@ import numpy as np
 import time
 import datetime
 import matplotlib.pyplot as plt
+from scipy import stats as stats
 
 start = time.clock()
 
+### This function will return a list of dates between the given dates.
 def get_date_range(date1,date2):
     datestr1 = str(date1.month) + "/" + str(date1.day) + "/" + str(date1.year)
     day1 = datetime.datetime.strptime(datestr1, "%m/%d/%Y")
     datestr2 = str(date2.month) + "/" + str(date2.day) + "/" + str(date2.year)
     day2 = datetime.datetime.strptime(datestr2, "%m/%d/%Y")
-    if day1 == day2:
-        return [day1]
-    if day1 < day2:
-        start = day1
-        end = day2
-    else:
-        start = day2
-        end = day1
+    startday = min(day1,day2)
+    endday = max(day1,day2)
     daterange = []
-    while start < end:
-        daterange.append(start)
-        start = start + datetime.timedelta(1)
-    daterange.append(end)
+    while startday < endday:
+        daterange.append(startday)
+        startday = startday + datetime.timedelta(1)
+    daterange.append(endday)
     return daterange
 
+### This is the main function that will analyse data.
 def generate_report():
     k = -1
-    bike_durations = {}
-    bike_rentals = {}
+    use_by_id = {}
     round_trips = 0
     trips_by_date = {}
     weekend_trips = 0
@@ -40,6 +36,8 @@ def generate_report():
     day_dict = {}
     for j in range(1,8):
         day_dict[j] = [[],[]]
+
+### Parse csv file and identify pertinent data from each entry
     with open('201508_trip_data.csv') as csvfile:
         data_table = csv.reader(csvfile)
         for entry in data_table:
@@ -48,68 +46,75 @@ def generate_report():
             bikeID = entry[8]
             tripDuration = entry[1]
 
-## collect data for most use bike
+    ### collect data for most use bike
             if k > -1:
-
                 try:
-                    bike_durations[bikeID] = bike_durations[bikeID]+ float(tripDuration)
+                    use_by_id[bikeID][0] = use_by_id[bikeID][0]+ \
+                    float(tripDuration)
                 except:
-                    bike_durations[bikeID] = float((tripDuration))
-                try:
-                    bike_rentals[bikeID] = bike_rentals[bikeID]+ 1
-                except:
-                    bike_rentals[bikeID] = 1
+                    use_by_id[bikeID] = [float((tripDuration)),1]
+                use_by_id[bikeID][1] = use_by_id[bikeID][1]+ 1
 
-## collect data for number of daily trips
-                startDate = datetime.datetime.strptime(entry[2], "%m/%d/%Y %H:%M")
-                endDate = datetime.datetime.strptime(entry[5], "%m/%d/%Y %H:%M")
+    ### collect data for number of daily trips
+                startDate = datetime.datetime.strptime(entry[2], \
+                                                       "%m/%d/%Y %H:%M")
+                endDate = datetime.datetime.strptime(entry[5], \
+                                                     "%m/%d/%Y %H:%M")
                 daterange = get_date_range(startDate,endDate)
                 for date in daterange:
                     try:
                         trips_by_date[date] = trips_by_date[date]+ 1
                     except:
                         trips_by_date[date] = 1
-                    
-
-                
-## collect data for prop of round trips
+                                  
+    ### collect data for prop of round trips
                 if startTerminal == endTerminal:
                     round_trips = round_trips + 1
             k = k + 1
 
-### analysis for most bike time used
+### analysis for most time and most number of rentals
     most_used_time = 0
     most_used_bike = 0
-    for entry in bike_durations:
-        if bike_durations[entry] > most_used_time:
-            most_used_time = bike_durations[entry]
-            most_used_bike = entry
-    print "Bike number ", most_used_bike, " saw the most use at ", most_used_time, " seconds."
     most_rentals = 0
     most_rented_bike = 0
-    for entry in bike_rentals:
-        if bike_rentals[entry] > most_rentals:
-            most_rentals = bike_rentals[entry]
+    for entry in use_by_id:
+        if use_by_id[entry][0] > most_used_time:
+            most_used_time = use_by_id[entry][0]
+            most_used_bike = entry
+        if use_by_id[entry][1] > most_rentals:
+            most_rentals = use_by_id[entry][1]
             most_rented_bike = entry
-    print "Bike number ", most_rented_bike, " saw the most rentals at ", most_rentals, " rentals."
+    print ""
+    print "Bike number ", most_used_bike, " saw the most use at ", \
+        most_used_time, " seconds."
+    print "Bike number ", most_rented_bike, " saw the most rentals at ",\
+        most_rentals, " rentals."
+    print ""
 
-### anlysis for likelihood of round trips. It is assumed that each ride is independent and that the sample size is over 1000 rides,
-### with at least this many rides the sampling distribution is essentially normal and using the test statistic cutoff of +/- 3
-### corresponds to a significnace level of about .05. 
-    rtProp = float(round_trips)/k
-    testStat = (rtProp - .5)/(np.sqrt(.5 *(1-.5)/k))
-    if abs(testStat) > 3:
+    
+### anlysis for likelihood of round trips. It is assumed that each ride
+### is independent. The test is conducted with significnace level of .05.
+    rtProp = float(round_trips)/k    
+    pval = stats.binom_test(round_trips,k,.5)
+
+    if abs(pval) < .05:
         if rtProp < .5:
-            print "Riders are significantly more likely to not make round trips"
+            print "Riders are significantly more likely to make", \
+                " one-way trips."
         else:
-            print "Riders are significantly more likely to make round trips"
+            print "Riders are significantly more likely to make round trips."
             
     else:
-        print "This data does not reflect a significant difference preference of round to one-way trips"
-    print "Round trips were made ", rtProp * 100, " percent of the time"
-    print "for a test statistic of ", testStat
+        print "This data does not reflect a significant difference", \
+            " in prefreence of round to one-way trips"
+    print "Round trips were made ", rtProp * 100, " percent of the time."
+    print "With a p-value of ", pval, " 'significant', in this case,"
+    print "means a confidence of about ", 100*(1-pval), "%."
+    print ""
 
-### Separate trips by day of the week, count weekend vs weekday     
+    
+### Separate trips by day of the week, count weekend vs weekday
+     
     for entry in trips_by_date:
         j = entry.isoweekday()
         day_dict[j][0] = day_dict[j][0] + [entry]
@@ -120,52 +125,57 @@ def generate_report():
     for j in range(6,8):
         for i in day_dict[j][1]:
             weekend_trips = weekend_trips + i    
-    print "there were ", weekend_trips, " trips taken on the weekend and ", weekday_trips, " trips taken on the weekdays."
+    print "there were ", weekend_trips, " trips taken on the weekend and ", \
+        weekday_trips, " trips taken on the weekdays."
 
-###Display Graph of number of trips by date, different makers for different days of the week
+### Display Graph of number of trips by date, 
+### different makers for different days of the week
 
     fig = plt.figure()
-    Monday = plt.plot(day_dict[1][0],day_dict[1][1], 'g^', label = 'Monday')
-    Tuesday = plt.plot(day_dict[2][0],day_dict[2][1], 'b^', label = 'Tuesday')
-    Wednesday = plt.plot(day_dict[3][0],day_dict[3][1], 'm^', label = 'Wednesday')
-    Thursday = plt.plot(day_dict[4][0],day_dict[4][1], 'r^', label = 'Thursday')
-    Friday = plt.plot(day_dict[5][0],day_dict[5][1], 'y^', label = 'Friday')
-    Saturday = plt.plot(day_dict[6][0],day_dict[6][1], 'ko', label = 'Saturday')
-    Sunday = plt.plot(day_dict[7][0],day_dict[7][1], 'co', label = 'Sunday')
-    plt.legend(bbox_to_anchor=(0., 1., 1., .01), loc=3, ncol=4, mode="expand", borderaxespad=0.)
-    end = time.clock()
-    print end - start
+    days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', \
+            'Saturday', 'Sunday']
+    icons = ['g^', 'b^', 'm^', 'r^', 'y^', 'ko', 'co']
+    for j in range (1,8):
+        plt.plot(day_dict[j][0], day_dict[j][1], \
+                 icons[j-1] , label = days[j-1])
+    plt.legend(bbox_to_anchor=(0., 1., 1., .01), loc=3, ncol=4, \
+                 mode="expand", borderaxespad=0.)
     plt.show()
-    fig.savefig("RidersByDay.png")
+    fig.savefig("BikeShareRidersByDay.png")
     plt.close()
-
-
-
-            
-
-    
-    
 
 generate_report()
 
+print ""
+
+print "Further Analysis:"
+
+print ""
+
+print "Difference in round trips:  There appears to be a very significant" 
+print "difference in the likelihood of riders to make round-trips."
+print "In particular they are much less likely to not make round trips." 
+print "This is likely due to people using the bikes for commute"
+print "rather than for pleasure."
+print ""
+
+print "Difference in Weekday vs. Weekend Usage: The data shows that almost 3.5" 
+print "times as many bikes are used on weekdays than on weekends."
+print "This further supports the idea that these bikes are mainly being used" 
+print "for commute, presumably to and from work."
+print ""
+
+print "Business Ramifications: It would be useful to increase business on the"
+print "weekends and/or encourage riders to use the bikes for"
+print "pleasure riding/excercise as well as for the commute they already use" 
+print "them for.  One solution might be to advertise and offer 'premium'"
+print "touring or mountin bikes that people could rent, perhaps for a "
+print "slightly higher price."
+
+print ""
+
 end = time.clock()
-print end - start
-
-
-### Further Analysis:
-
-### Difference in round trips:  There appears to be a very significant difference in the likelihood of riders to make round-trips.
-### In particular they are much less likely to not make round trips. This is likely due to people using the bikes for commute
-### rather than for pleasure.
-
-### Difference in Weekday vs. Weekend Usage: The data shows that almot 3.5 times as many bikes are used on weekdays than on weekends.
-### This further supports the idea that these bikes are mainly being used for commute, presumably to and from work. 
-
-### Business Ramifications: It would be useful to increase business on the weekends and/or encourage riders to use the bikes for
-### pleasure riding/excercise as well as for the commute they already use them for.  One solution might be to advertise and offer "nicer"
-### touring or mountin bikes that people could rent, perhaps for a slightly higher price. 
-
-
+print "run time:", end - start
 
 
 
